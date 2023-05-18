cbf728c0265efc5c38cef13ba07985f44a631099
Y Kassabi <22109127+ykassabi@users.noreply.github.com>
2023-05-18 20:17:54 +0200

    add english page

---
+++ js/index-en.js
@@ -0,0 +1,50 @@
+"use strict";
+window.addEventListener('load', function() {
+	AOS.init({
+		easing: 'ease-in-out-sine'
+	});
+});
+
+window.addEventListener('load', function() {
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------
+//								COUNT UP SCRIPT
+//------------------------------------------------------------------------------------
+
+var benefits_2col_counter__0 = $('#benefits-2col-counter--0').waypoint({
+	handler: function(direction) {
+		$(this.element).find('.count-up-data').each(function(i, el){
+			var endVal = el && Number.isInteger(el.textContent * 1) ? el.textContent * 1 : 100 ;
+			$(el ).countup({endVal: endVal, options: { separator : ''}});
+		});
+        benefits_2col_counter__0[0].disable();
+	},
+	offset: '90%'
+});
+//------------------------------------------------------------------------------------
+//								COUNT UP SCRIPT
+//------------------------------------------------------------------------------------
+
+var benefits_2col_counter__1 = $('#benefits-2col-counter--1').waypoint({
+	handler: function(direction) {
+		$(this.element).find('.count-up-data').each(function(i, el){
+			var endVal = el && Number.isInteger(el.textContent * 1) ? el.textContent * 1 : 100 ;
+			$(el ).countup({endVal: endVal, options: { separator : ''}});
+		});
+        benefits_2col_counter__1[0].disable();
+	},
+	offset: '90%'
+});
+
+
+});
