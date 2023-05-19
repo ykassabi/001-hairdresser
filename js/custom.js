3b8566465be18677c94b6fbb39bae215f897c62e
Y Kassabi <22109127+ykassabi@users.noreply.github.com>
2023-05-19 10:00:35 +0200

    optimization of the disolay

--- js/custom.js
+++ js/custom.js
@@ -139,6 +139,9 @@ $('.masonry-filter').MasonryFilter({
 });
 var pAgree = '1';
 window.addEventListener('load', function() {
+	$('a.smooth').smoothScroll({speed: 800});
+});
+window.addEventListener('load', function() {
 		var rellax = new Rellax('.parallax', {
 			center: true
 		});
